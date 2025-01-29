5c376393af5928128658be385e381b1ff972eaf9
Mazon <amazona01@educantabria.es>
2025-01-29 12:59:24 +0100

    Update nginx.sh

--- scripts_servicios/nginx.sh
+++ scripts_servicios/nginx.sh
@@ -7,8 +7,8 @@ sudo ufw allow 'Nginx HTTP'
 sudo git clone https://github.com/amazona01/AWSCLI.git
 
 #mover configuraciones
-sudo mv AWSCLI/configuraciones_servicios/wordpress /etc/nginx/sites-available/
-sudo mv AWSCLI/configuraciones_servicios/default /etc/nginx/sites-available/
+sudo mv AWSCLI/configuraciones_servicios/nginx/wordpress /etc/nginx/sites-available/
+sudo mv AWSCLI/configuraciones_servicios/nginx/default /etc/nginx/sites-available/
 
 #symlinks 
 sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
