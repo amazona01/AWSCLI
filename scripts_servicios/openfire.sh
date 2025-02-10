45ba9fe3e0a78d8b2cfdcc44f330090b138efe2d
Mazon <amazona01@educantabria.es>
2025-02-10 21:18:12 +0100

    Update wordpress2.sh

--- scripts_servicios/openfire.sh
+++ scripts_servicios/openfire.sh
@@ -4,20 +4,14 @@
 sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 
 # Instalar paquetes necesarios
-sudo DEBIAN_FRONTEND=noninteractive apt install -y openjdk-17-jre default-jre-headless wget mysql-client
-
-# Instalar OpenJDK 11 y MySQL
-sudo apt-get --fix-broken install -y
+sudo DEBIAN_FRONTEND=noninteractive apt install -y openjdk-17-jre default-jre-headless wget mysql-client && sudo apt-get --fix-broken install -y
 
 # Descargar Openfire
 wget https://download.igniterealtime.org/openfire/openfire_4.9.2_all.deb -O openfire_4.9.2_all.deb
 
 
 # Instalar Openfire
-sudo dpkg -i openfire_4.9.2_all.deb
-
-# Asegurarse de que no queden dependencias rotas
-sudo apt-get --fix-broken install -y
+sudo dpkg -i openfire_4.9.2_all.deb && sudo apt-get --fix-broken install -y
 
 # Habilitar y arrancar el servicio de Openfire
 sudo systemctl enable openfire
