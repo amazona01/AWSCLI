944950f16f8a205685a627b55884b6b9e6b57639
Mazon <amazona01@educantabria.es>
2025-02-10 20:34:09 +0100

    fix

--- scripts_servicios/clustersql.sh
+++ scripts_servicios/clustersql.sh
@@ -21,7 +21,23 @@ if ! dpkg -l | grep -q mysql-server; then
 fi
 
 # 3. Configure MySQL
+if [ "$role" = "primary" ]; then
 server_id=1
+apt update && DEBIAN_FRONTEND=noninteractive apt install  -y mysql-server
+
+sudo sed -i "s/^bind-address\s*=.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo systemctl restart mysql
+
+sudo mysql -u root -p_Admin123 -e "CREATE DATABASE openfire;"
+sudo mysql -u root -p_Admin123-e "source /home/ubuntu/openfire.sql"
+sudo mysql -u root -p_Admin123 -e "CREATE USER 'openfire'@'%' IDENTIFIED BY '_Admin123';"
+sudo mysql -u root -p_Admin123 -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON openfire.* TO 'openfire'@'%';"
+sudo mysql -u root -p_Admin123 -e "FLUSH PRIVILEGES;"
+
+chmod +x /home/ubuntu/backups.sh
+mkdir /home/ubuntu/backups
+(crontab -l 2>/dev/null; echo "0 3 * * * /home/ubuntu/backups.sh >/dev/null 2>&1") | crontab -
+fi
 if [ "$role" = "secondary" ]; then
     server_id=2
 fi
@@ -40,9 +56,8 @@ systemctl restart mysql
 systemctl enable mysql
 
 # 5. Basic Security Setup
-mysql <<EOF
+mysql -u root -p_Admin123 <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$db_password';
-DELETE FROM mysql.user WHERE User='';
 CREATE DATABASE IF NOT EXISTS $db_name;
 FLUSH PRIVILEGES;
 EOF
@@ -62,8 +77,7 @@ EOF
 elif [ "$role" = "secondary" ]; then
     # Wait for primary to be ready
     echo "Waiting for primary MySQL server to be ready..."
-    timeout 300 bash -c 'until nc -z $primary_ip 3306; do sleep 10; done'
-
+    timeout 300 bash -c "until nc -z $primary_ip 3306; do sleep 10; done"
     # Copy master status from primary
     scp -o StrictHostKeyChecking=no -i $ssh_key_path ubuntu@$primary_ip:/tmp/master_status.txt /tmp/
 
