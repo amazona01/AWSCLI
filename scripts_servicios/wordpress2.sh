396d0a49d00cf9b8c606f6644b4ada0d816b3d74
Mazon <amazona01@educantabria.es>
2025-02-10 13:25:07 +0100

    wordpress rapido

--- scripts_servicios/wordpress2.sh
+++ scripts_servicios/wordpress2.sh
@@ -1,13 +1,15 @@
+sudo sed -i '1d' /var/www/html/wp-config.php
 
-echo "
-if(isset(\$_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    \$list = explode(',', \$_SERVER['HTTP_X_FORWARDED_FOR']);
-    \$_SERVER['REMOTE_ADDR'] = \$list[0];
-}
-\$_SERVER['HTTP_HOST'] = 'nginxequipo45.duckdns.org';
-\$_SERVER['REMOTE_ADDR'] = 'nginxequipo45.duckdns.org';
-\$_SERVER['SERVER_ADDR'] = 'nginxequipo45.duckdns.org';
-" | sudo tee -a /var/www/html/wp-config.php
+sudo sed -i '1i\
+<?php if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {\
+    $list = explode(",", $_SERVER["HTTP_X_FORWARDED_FOR"]);\
+    $_SERVER["REMOTE_ADDR"] = $list[0];\
+}\
+$_SERVER["HTTP_HOST"] = "nginxequipo45.duckdns.org";\
+$_SERVER["REMOTE_ADDR"] = "nginxequipo45.duckdns.org";\
+$_SERVER["SERVER_ADDR"] = "nginxequipo45.duckdns.org";\
+' /var/www/html/wp-config.php
+
 
 sudo scp -i clave.pem -o StrictHostKeyChecking=no ubuntu@10.218.1.10:/home/ubuntu/certwordpress/* /home/ubuntu/
 sudo cp /home/ubuntu/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
