5d55478e39f6138a533e36e7efa008cfc4da6f4c
Mazon <amazona01@educantabria.es>
2025-02-11 08:49:54 +0100

    scripts copiados a data user

---
+++ aws-data-user/nginx.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#cambiar dominios
+wordpress=nginxequipo45
+openfire=openfire-equipo45
+#cambiar token
+token=0b4bb411-ab26-4464-8a16-0d373fa6bf9c 
+#cambiar alumno
+alumno=amazona01
+
+mkdir -p "/home/ubuntu/duckdns/"
+cd "/home/ubuntu/duckdns/"
+
+sudo apt update && sudo  DEBIAN_FRONTEND=noninteractive apt install nginx -y
+
+# Crear script para actualizar la ip dinamicamente
+echo "echo url=\"https://www.duckdns.org/update?domains=$wordpress&token=$token&ip=\" | curl -k -o /home/ubuntu/duckdns/duck.log -K -" > "/home/ubuntu/duckdns/duck.sh"
+chmod 700 "/home/ubuntu/duckdns/duck.sh"
+
+echo "echo url=\"https://www.duckdns.org/update?domains=$openfire&token=$token&ip=\" | curl -k -o /home/ubuntu/duckdns/duck.log -K -" > "/home/ubuntu/duckdns/duck2.sh"
+chmod 700 "/home/ubuntu/duckdns/duck2.sh"
+# Añadir al crontab
+(crontab -l 2>/dev/null; echo "*/1 * * * * /home/ubuntu/duckdns/duck.sh >/dev/null 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/1 * * * * /home/ubuntu/duckdns/duck2.sh >/dev/null 2>&1") | crontab -
+
+sleep 120
+
+#Instalación de Nginx
+sudo apt update && sudo  DEBIAN_FRONTEND=noninteractive apt install nginx-full python3-pip -y
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+pip install certbot-dns-duckdns
+snap install certbot-dns-duckdns
+
+sudo snap set certbot trust-plugin-with-root=ok
+sudo snap connect certbot:plugin certbot-dns-duckdns
+
+#mover configuraciones
+sudo mv /home/ubuntu/default /etc/nginx/
+sudo mv /home/ubuntu/nginx.conf /etc/nginx/nginx.conf
+#Restart Nginx
+sudo systemctl stop nginx
+
+sudo certbot certonly  --non-interactive --agree-tos --email $alumno@educantabria.es --preferred-challenges dns --authenticator dns-duckdns --dns-duckdns-token $token --dns-duckdns-propagation-seconds 60 -d "$wordpress.duckdns.org"
+sudo certbot certonly  --non-interactive --agree-tos --email $alumno@educantabria.es --preferred-challenges dns --authenticator dns-duckdns --dns-duckdns-token $token --dns-duckdns-propagation-seconds 120 -d "$wordpress.duckdns.org"
+sudo certbot certonly  --non-interactive --agree-tos --email $alumno@educantabria.es --preferred-challenges dns --authenticator dns-duckdns --dns-duckdns-token $token --dns-duckdns-propagation-seconds 60 -d "$openfire.duckdns.org"
+sudo certbot certonly  --non-interactive --agree-tos --email $alumno@educantabria.es --preferred-challenges dns --authenticator dns-duckdns --dns-duckdns-token $token --dns-duckdns-propagation-seconds 120 -d "$openfire.duckdns.org"
+sudo certbot certonly --non-interactive --agree-tos --email $alumno@educantabria.es --preferred-challenges dns --authenticator dns-duckdns --dns-duckdns-token $token --dns-duckdns-propagation-seconds 60 -d "*.$openfire.duckdns.org"
+sudo certbot certonly --non-interactive --agree-tos --email $alumno@educantabria.es --preferred-challenges dns --authenticator dns-duckdns --dns-duckdns-token $token --dns-duckdns-propagation-seconds 120 -d "*.$openfire.duckdns.org"
+
+mkdir /home/ubuntu/certwordpress
+mkdir -p /home/ubuntu/certopenfire/wildcard
+
+sudo cp /etc/letsencrypt/live/$wordpress.duckdns.org/* /home/ubuntu/certwordpress/
+sudo cp /etc/letsencrypt/live/$openfire.duckdns.org/* /home/ubuntu/certopenfire/
+sudo cp /etc/letsencrypt/live/$openfire.duckdns.org-0001/* /home/ubuntu/certopenfire/wildcard/
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu
+sudo chmod -R 770 /home/ubuntu
+
+sudo systemctl start nginx
+
+#Borrar
+rm -rf mensagl
